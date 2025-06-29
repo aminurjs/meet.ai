744d10668b5c3af05ff147c7c783750f70086347
Aminur Rahman <dev.aminur@gmail.com>
2025-06-29 13:35:07 +0600

    Implement authentication layout and views for sign-in and sign-up

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-views";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <SignInView />
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-views";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUpView />
+    </div>
+  );
+};
+
+export default SignUpPage;
